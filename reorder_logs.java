f957f7f72a9f9c55bb490ec88bf2be74b62ed19f
Advait Churi <advaitchuri@Advaits-MacBook-Pro.local>
2024-08-28 20:18:05 -0700

    String-4

---
+++ reorder_logs.java
@@ -0,0 +1,34 @@
+class Solution {
+    public String[] reorderLogFiles(String[] logs) {
+        if(logs == null || logs.length == 0){
+            return logs;
+        }
+
+        Arrays.sort(logs, (log1,log2) -> {
+            String [] strArr1 = log1.split(" ", 2);
+            String [] strArr2 = log2.split(" ", 2);
+            
+            boolean isDigit1 = Character.isDigit(strArr1[1].charAt(0));
+            boolean isDigit2 = Character.isDigit(strArr2[1].charAt(0));
+
+            if(!isDigit1 && !isDigit2){
+                int cmp = strArr1[1].compareTo(strArr2[1]);
+                if(cmp == 0){
+                    return strArr1[0].compareTo(strArr2[0]);
+                }
+                return cmp;
+            }
+            else if(!isDigit1 && isDigit2){
+               return 1;
+            }
+            else if(isDigit1 && !isDigit2){
+               return -1;
+            }
+            else{
+                return 0;
+            }
+        });
+
+        return logs;
+    }
+}
