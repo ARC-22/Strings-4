106451ea83a487ac20da8d236ecd4c7d192b403b
ARC-22 <advaitchuri22@gmail.com>
2024-09-18 19:30:42 -0700

    read n char

--- Read_N_char.java
+++ Read_N_char.java
@@ -1,5 +1,5 @@
 // Time Complexity : O(n) 
-// Space Complexity : O(n)
+// Space Complexity : O(n)   
 // Did this code successfully run on Leetcode : NO //premium question
 // Any problem you faced while coding this : No
 
